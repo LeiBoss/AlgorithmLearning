3ddfce6f7aa256e74a1a5aa92ec3bbcd1f5e6641
LeiYW <1902862824@qq.com>
2019-01-06 22:50:46 +0800

    '插入排序'

---
+++ AlgorithmLearning/AlgorithmLearning/InsertionSort.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AlgorithmLearning
+{
+    /// <summary>
+    /// 插入排序
+    /// </summary>
+    public class InsertionSort
+    {
+        /// <summary>
+        /// 升序
+        /// </summary>
+        /// <param name="nums"></param>
+        public static void AscSort(int[] nums)
+        {
+            for (int i = 1; i < nums.Length; i++)
+            {
+                int key = nums[i];
+                int j = i - 1;
+                while (j >= 0 && nums[j] > key)
+                {
+                    nums[j + 1] = nums[j];
+                    j = j - 1;
+                }
+                nums[j + 1] = key;
+            }
+        }
+    }
+}
---
+++ AlgorithmLearning/AlgorithmLearning/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace AlgorithmLearning
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = { 5, 2, 4, 6, 1, 3 };
+            InsertionSort.AscSort(nums);
+            Console.WriteLine(string.Join(",",nums));
+            Console.ReadKey();
+        }
+    }
+}
